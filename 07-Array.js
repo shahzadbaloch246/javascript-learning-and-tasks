8862ba3826a2f26292020932d5769b9c64799314
Shahzad Baloch <shahzadbaloch2462006@gmail.com>
2026-07-07 18:36:41 +0500

    Added strings task

---
+++ 07-Array.js
@@ -0,0 +1,10 @@
+let arr=[1,true,3,"shah"]
+//console.log(arr)
+//arr.push(5)
+//arr.pop()
+////arr.unshift(9)
+arr.shift(true)
+//console.log(arr)
+let arr1=[6,6,6]
+arr1=arr.join()
+console.log(arr1)
